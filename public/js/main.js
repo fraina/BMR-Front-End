fc758b9a22618c521c1ec22bb07a8494dadff661
Fraina <jscaem@gmail.com>
2014-09-05 17:05:36 +0800

    加入 MediaElementJS, style 待調整

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,57 @@ require({
 	baseUrl : "js",
 	paths: {
 			"jquery": "lib/jquery/jquery-git2",
+			"jqueryui": "lib/jquery-ui/jquery-ui.min",
 			"angular": "lib/angularjs/angular.1.3.x.min",
 			"angular-route": "lib/angularjs/angular-route.min",
-			"angularAMD": "lib/angularjs/angularAMD"
+			"angularAMD": "lib/angularjs/angularAMD",
+
+			"coverflowjs": "vendor/coverflow/coverflow",
+			"renderer3d": "vendor/coverflow/renderer.3d",
+			"rendererClassic": "vendor/coverflow/renderer.classic",
+			"supportCore": "vendor/coverflow/support.core",
+			"supportTransform": "vendor/coverflow/support.transform3d",
+
+			"mediaElement": "vendor/media-element/mediaelement-and-player.min"
 	},
 
 	shim: {
-		'angularAMD': ['angular'],
-		'angular-route': ['angular']
+		"angularAMD": ['angular'],
+		"angular-route": ['angular'],
+		"renderer3d" : [ "jquery", "rendererClassic" ],
+		"rendererClassic" : [ "jquery" ],
+		"supportCore" : [ "jquery" ],
+		"supportTransform" : [ "jquery" ],
+		"coverflowjs" : {
+			deps : [
+				"jquery",
+				"jqueryui",
+				"renderer3d",
+				"rendererClassic",
+				"supportCore",
+				"supportTransform"
+			]
+		}
 	},
 
 	deps: ['app']
 });
+
+require([
+	'jquery',
+	'coverflowjs',
+	'mediaElement'
+], function (jquery, coverflowjs, mediaElement) {
+  $('#coverflow').coverflow();
+
+  $('#audio-player').mediaelementplayer({
+    alwaysShowControls: true,
+    features: ['playpause','progress','volume'],
+    audioVolume: 'horizontal',
+    audioWidth: 450,
+    audioHeight: 70,
+    iPadUseNativeControls: true,
+    iPhoneUseNativeControls: true,
+    AndroidUseNativeControls: true
+  });
+})
